c6f596fda7c9d475091a60906a4df592a3808f9b
vetal <vy@napoleonit.ru>
2020-01-15 14:57:20 +0500

    + add base ui table component

---
+++ src/components/ui/table/index.vue
@@ -0,0 +1,17 @@
+<template lang="pug">
+    .ui-table
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { ThisTypedComponentOptionsWithRecordProps } from 'vue/types/options'
+
+interface Data {}
+interface Methods {}
+interface Computed {}
+interface Props {}
+
+export default Vue.extend({
+    name: 'UiTable'
+} as ThisTypedComponentOptionsWithRecordProps<Vue, Data, Methods, Computed, Props>)
+</script>
