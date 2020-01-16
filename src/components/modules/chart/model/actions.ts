e3af247e7a2a8e33c3ed84d591143191b35fd950
vetal <vy@napoleonit.ru>
2020-01-16 13:09:17 +0500

    + write marketsHashTable to state

--- src/components/modules/chart/model/actions.ts
+++ src/components/modules/chart/model/actions.ts
@@ -1,10 +1,13 @@
 import { ActionTree } from 'vuex'
 import { IMarketsResponse } from '../../../../requests/markets/interfaces'
 import { getMarkets } from '../../../../requests/markets/markets'
+import { marketsHashTableAdapter } from './adapter'
 
 export const chartActions: ActionTree<any, any> = {
   async getMarkets({ commit }) {
     const markets: IMarketsResponse = await getMarkets()
+    const marketHash = marketsHashTableAdapter(markets.data)
     commit('markets', markets.data)
+    commit('marketsHashTable', marketHash)
   }
 }
