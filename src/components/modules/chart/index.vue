7cc1cb8ee63f84738eed9ce6e41b9ca1fdc62f9e
vetal <vy@napoleonit.ru>
2020-01-17 11:25:21 +0500

    + add logic show and update prices

--- src/components/modules/chart/index.vue
+++ src/components/modules/chart/index.vue
@@ -1,17 +1,56 @@
 <template lang="pug">
     .module-chart
+      .module-chart__item(v-for="(value, key) in marketsHashTable", :key="key")
+        .module-chart__row.module-chart__row_name {{ key }}
+        .module-chart__row.module-chart__row_price {{ changedPrices[key] ? showChangedMarket(key, changedPrices[key]) : value.priceUsd }}
+        br
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 import { ThisTypedComponentOptionsWithRecordProps } from 'vue/types/options'
+import { moduleName, store } from './model/store'
+import { createNamespacedHelpers } from 'vuex'
+import { subscribePriceChanges } from './model/mediator'
+import { IMarketHashTable } from './interfaces'
 
+type ChangedPriceItem = { changedKey: string; changedPrice: string }
 interface Data {}
-interface Methods {}
-interface Computed {}
+interface Methods {
+  getMarkets: () => Promise<void>
+  changeMarkets: (changedPriceItem: ChangedPriceItem) => Promise<void>
+}
+interface Computed {
+  marketsHashTable: IMarketHashTable
+}
 interface Props {}
 
+const { mapGetters, mapActions } = createNamespacedHelpers(moduleName)
+
 export default Vue.extend({
-  name: 'ModuleChart'
+  name: 'ModuleChart',
+  beforeCreate() {
+    this.$store.registerModule(moduleName, store)
+  },
+  beforeDestroy() {
+    this.$store.unregisterModule(moduleName)
+  },
+  async created() {
+    await this.getMarkets()
+    subscribePriceChanges(
+      `${moduleName}/pricesChanged`,
+      Object.keys(this.marketsHashTable).join(',')
+    )
+  },
+  methods: {
+    showChangedMarket(changedKey: string, changedPrice: string) {
+      this.changeMarkets({ changedKey, changedPrice })
+      return changedPrice
+    },
+    ...mapActions(['getMarkets', 'changeMarkets'])
+  },
+  computed: {
+    ...mapGetters(['marketsHashTable', 'changedPrices'])
+  }
 } as ThisTypedComponentOptionsWithRecordProps<Vue, Data, Methods, Computed, Props>)
 </script>
