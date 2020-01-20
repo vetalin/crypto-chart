a93a4df1be6d20ef7fabe5c452d7e7537e7bfbf6
vetal <vy@napoleonit.ru>
2020-01-20 12:06:22 +0500

    Updates

---
+++ precache-manifest.d33ea119c7e120bdf61d1de3e42d5f03.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ec173726034f94e03c3",
+    "url": "/css/app.a9a9296d.css"
+  },
+  {
+    "revision": "323f085e5fb99003aa3906ad600dcdb8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5ec173726034f94e03c3",
+    "url": "/js/app.6cde71df.js"
+  },
+  {
+    "revision": "0dd24b5661489e277b42",
+    "url": "/js/chunk-vendors.95cd9374.js"
+  },
+  {
+    "revision": "f4a1482f28d885429db9806912f3f777",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
