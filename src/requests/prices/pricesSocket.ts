2e26d8bdcaaeef81e528c2c9345ba0b96bb69951
vetal <vy@napoleonit.ru>
2020-01-20 11:21:24 +0500

    + add todo comment

--- src/requests/prices/pricesSocket.ts
+++ src/requests/prices/pricesSocket.ts
@@ -6,5 +6,10 @@ export const initPricesListener = (
 ): WebSocket => {
   const pricesWS = new WebSocket(`wss://ws.coincap.io/prices?assets=${markets}`)
   pricesWS.onmessage = listener
+  pricesWS.onerror = e => console.error(e)
+  pricesWS.onclose = () => {
+    console.log('price socket closed')
+    // TODO: maybe reopen socket
+  }
   return pricesWS
 }
